37631d4831fbd4943eb2fcb04a1a45c5b75d0b52
Camille Dewavrin <camilledewavrin@hotmail.fr>
2014-06-02 22:05:01 +0200

    MAJ script

--- style/js/monScript.js
+++ style/js/monScript.js
@@ -1,23 +1,8 @@
 $(function() {
-	/* Third demo */
-	/*$('#demo3_thumbs').desoSlide({
-		main: {
-			container: '#demo3_main_image',
-			cssClass: 'img-responsive'
-		},
-		overlay: 'hover',
-		caption: true,
-		controls: {
-			enable: false,
-			keys: false
-		}
-	});*/
-
 	$('.bxslider').bxSlider({
  		 pagerCustom: '#bx-pager',
  		 auto : true,
- 		 adaptiveHeight: true,
- 		 slideMargin: 20
+ 		 adaptiveHeight: true
 	});
 
 
