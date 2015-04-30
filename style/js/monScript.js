a81e70e199a1ce28d8a0e124a990b74865298791
Camille Dewavrin <camilledewavrin@hotmail.fr>
2015-05-01 00:06:39 +0200

    Ajout barre fixe

--- style/js/monScript.js
+++ style/js/monScript.js
@@ -2,18 +2,15 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var y = $(window).scrollTop();
  
-        /*if( y > 700 )
+        if( y > 680 )
         {
-            $("#Home").css({'background-color':'red'});
-            $(".navbar-default .navbar-nav > li > a").css({'color':'#FFF'});
+            $("#navbar").fadeIn("slow");
+            $("#navbar").css({'display':'initial'});
+            //$(".navbar-default .navbar-nav > li > a").css({'color':'#FFF'});
             //$('#navbar').animate({ 'height': '60'}, {duration : 200, queue : false});
         } else {
-            $(".#Home").css({'background-color':'blue'});
-            $(".navbar-default .navbar-nav > li > a").css({'color':'#FFF'});
-            //$('#navbar').animate({ 'height': '51'}, {duration : 200, queue : false});
-
-        }*/
- 
-       // $('#result').html(y);
+            $("#navbar").fadeOut("slow");
+            //$("#navbar").css({'display':'none'});
+        }
     });
 });
